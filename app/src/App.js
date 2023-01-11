bdfa2e50cef4543fd4939e48e43ee9e39794a426
Alina <alina.bodnar@zynksoftware.com>
2023-01-11 23:40:24 +0200

    solved conflicts

--- app/src/App.js
+++ app/src/App.js
@@ -62,7 +62,7 @@ function App() {
       <EditInterestsRoutes />
       <RoutesCoordinator />
       <AcceptedStudentsRoutes />
-        <GestionAssignmentsRoutes/>
+      <GestionAssignmentsRoutes/>
     </React.Fragment>
   }
 
